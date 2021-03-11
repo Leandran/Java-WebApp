0c0a628cfbf79def401cd039d52582b34dc01a8b
Leandran <41592532+Leandran@users.noreply.github.com>
2021-03-12 01:04:56 +0200

    Add files via upload

---
+++ WebApplication/src/com/MyServlet.java
@@ -0,0 +1,56 @@
+package com;
+
+import java.io.IOException;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	//protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//PrintWriter out= response.getWriter();
+		//out.print("<h1>Hello world Service Method</h1>");
+	//}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out= response.getWriter();
+		String name = request.getParameter("fname");
+		out.print("Hello world doGet Method");
+		out.print("<br> Hello " + name );
+	}  
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out= response.getWriter();
+		String name = request.getParameter("fname");
+		out.print("Hello world doPost Method");
+		out.print("<br> Hello " + name );
+	}
+
+}
